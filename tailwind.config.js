06fcc56a4a36b1e9f8e91992f9f4557422c2000e
Martine23FT <martinemidttveit@hotmail.com>
2024-03-22 18:23:45 +0100

    Making the auctions page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./**/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#222222",
+        secondary: "#C8E2EB",
+        light: "#C0E8EB",
+        yellow: "#FFE68C",
+        orange: "#F5841A",
+        background: "#F7F7F7",
+      },
+    },
   },
   plugins: [],
 }
