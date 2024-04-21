4cb5f18099b5737f5498633189289a899df28e77
Martine Midttveit <martinemidttveit@hotmail.com>
2024-04-21 03:42:35 +0200

    Changes

--- JAVASCRIPT/galleryObject.js
+++ JAVASCRIPT/galleryObject.js
@@ -1,22 +1,28 @@
-export default function galleryObject () {
-    const container = document.querySelector('.mobile-media-gallery');
+export default function galleryObject() {
+    const container = document.querySelector('#mobile-gallery');
+    const placeholder = document.querySelector('#mobile-placeholder');
+    const listingImage = document.querySelector('#listingImage');
+    const uploadButton = document.querySelector('#uploadButton');
 
-    const galleryPlaceholder = document.createElement('div');
-    galleryPlaceholder.setAttribute('class', 'w-full h-80 md:h-96 flex items-center justify-center rounded relative bg-grey');
+    uploadButton.addEventListener('click', () => {
+        const imageUrl = listingImage.value.trim();
+        if (imageUrl !== '') {
+            const galleryObjects = document.createElement('figure');
+            galleryObjects.setAttribute('class', 'w-full h-80 md:h-96 flex items-center justify-center rounded relative gallery-object');
 
-    const placeholderIcon = document.createElement('i');
-    placeholderIcon.setAttribute('class', 'far fa-image text-4xl text-gray-500'); 
+            const galleryImage = document.createElement('img');
+            galleryImage.setAttribute('class', 'object-cover h-full w-full rounded');
+            galleryImage.src = imageUrl; 
+            galleryImage.alt = 'Uploaded Image'; 
 
-    const galleryObjects = document.createElement('figure');
-    galleryObjects.setAttribute('class', 'w-full h-80 md:h-96 flex items-center justify-center rounded relative gallery-object');
+            galleryObjects.appendChild(galleryImage); 
+            container.appendChild(galleryObjects); 
 
-    const galleryImage = document.createElement('img');
-    galleryImage.setAttribute('class', 'object-cover h-full w-full rounded');
-    galleryImage.src = '';
-    galleryImage.alt = '';
+            saveLocalStorage(imageUrl);
+            listingImage.value = '';
 
-    galleryPlaceholder.append(placeholderIcon);
-    galleryObjects.append(galleryImage);
-
-    container.append(galleryPlaceholder, galleryObjects);
+            placeholder.classList.add('hidden');
+        }
+    });
 }
+
