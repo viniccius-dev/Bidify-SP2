733b177b44a895745e401d25e5ec6748611a5689
Martine23FT <martinemidttveit@hotmail.com>
2024-04-25 22:54:02 +0200

    Changes

--- src/js/login/seePassword.js
+++ src/js/login/seePassword.js
@@ -15,10 +15,8 @@ export default function seePassword() {
   toggleButton.addEventListener("click", function() {
     if (passwordInput.type === "password") {
       passwordInput.type = "text";
-      confirmPassword.type = "text";
     } else {
       passwordInput.type = "password";
-      confirmPassword.type = "password";
     }
   });
 }
