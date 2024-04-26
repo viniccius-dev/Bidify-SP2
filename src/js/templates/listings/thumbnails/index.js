df958c8759eb83025afc7e06eaff0791d92bbc00
Martine23FT <martinemidttveit@hotmail.com>
2024-04-26 03:18:18 +0200

    Changes

--- src/js/templates/listings/thumbnails/index.js
+++ src/js/templates/listings/thumbnails/index.js
@@ -9,7 +9,7 @@ export default function thumbnail(data) {
     a.href = `pages/listing/?id=${data.id}`;
     figure.appendChild(a);
 
-    const img = createEle("img", "rounded w-full object-cover listing-image hover:opacity-90 duration-300 h-44 sm:h-80 md:h-96 xl:h-80 2xl:h-96");
+    const img = createEle("img", "rounded w-full object-cover listing-image hover:opacity-90 duration-100 h-44 sm:h-80 md:h-96 xl:h-80 2xl:h-96");
     img.src = data.media[0].url;
     img.alt = data.media[0].alt ? data.media[0].alt : `${data.title} - a bidlify listing`;
     a.appendChild(img);
